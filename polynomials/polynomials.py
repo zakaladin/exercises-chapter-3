875ccbf243f2ce021354de6fe7ce9bb8889552a8
Zak Aladin <zak.aladin21@imperial.ac.uk>
2023-01-25 13:19:43 +0000

    Added __sub__, __mul__, __call__, __pow__

--- polynomials/polynomials.py
+++ polynomials/polynomials.py
@@ -1,4 +1,4 @@
-from numbers import Number
+from numbers import Number, Integral
 
 
 class Polynomial:
@@ -50,3 +50,64 @@ class Polynomial:
 
     def __radd__(self, other):
         return self + other
+
+    def __sub__(self, other):
+
+        if isinstance(other, Polynomial):
+            common = min(self.degree(), other.degree()) + 1
+            coefs = tuple(a - b for a, b in zip(self.coefficients[:common],
+                                                other.coefficients[:common]))
+            coefs += self.coefficients[common:] + other.coefficients[common:]
+
+            return Polynomial(coefs)
+
+        elif isinstance(other, Number):
+            return Polynomial((self.coefficients[0] - other,)
+                              + self.coefficients[1:])
+
+        else:
+            return NotImplemented
+
+    def __rsub__(self, other):
+        return Polynomial(() + (-i,) for i in (self-other).coefficients)
+
+    def __mul__(self, other):
+
+        if isinstance(other, Polynomial):
+            coefs = ()
+            for k in range(self.degree()*other.degree()+1):
+                x=0
+                for i,j in range(self.degree()*other.degree()+1): 
+                    if i+j == k:
+                        y = self.coefficients[i] + other.coefficients[j]
+                    x += y
+                coefs += (x,)
+            return Polynomial(coefs)
+
+        elif isinstance(other, Number):
+            return Polynomial(tuple(x * other for x in self.coefficients))
+
+        else:
+            return NotImplemented
+
+    def __rmul__(self, other):
+        return self * other
+
+    def __pow__(self, other):
+
+        if isinstance(other, Integral):
+            return self ** other
+        
+        else:
+            return NotImplemented
+
+    def __call__(self, other):
+        
+        if isinstance(other, Number):
+            new_tuple = tuple(pow(Number,d) * x for d,x in enumerate(self.coefficients))
+            return sum(new_tuple)
+
+        else:
+            return NotImplemented
+
+    
